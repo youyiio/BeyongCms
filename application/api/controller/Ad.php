d30e9b043ed0006653b91d9d6712fdca38f7780b
cmqcmq <chenmq@jifenbang.net>
2021-10-29 17:32:19 +0800

    广告接口，修复文章接口

--- application/api/controller/Ad.php
+++ application/api/controller/Ad.php
@@ -5,7 +5,9 @@ namespace app\api\controller;
 use app\api\controller\Base;
 use app\common\library\ResultCode;
 use app\common\model\cms\AdModel;
+use app\common\model\cms\AdServingModel;
 use app\common\model\cms\AdSlotModel;
+use app\common\model\ImageModel;
 use PhpOffice\PhpSpreadsheet\Calculation\Statistical\Distributions\F;
 use think\Validate;
 
@@ -56,23 +58,39 @@ class Ad extends Base
             return ajax_error(ResultCode::E_PARAM_VALIDATE_ERROR, $validate->getError());
         };
 
-        $page = $params["page"];
-        $size = $params["size"];
-        $query = $params["filters"];
+        $page = $params['page'];
+        $size = $params['size'];
+        $filters = $params["filters"];
+        $title = $filters['keyword']?? '';
+        $soltIds = $filters['soltIds']?? '';
 
-        $AdModel = new AdModel();
-
-        $pageConfig = [
-            'page' => $page,
-            'query' => $query
-        ];
-        $list = $AdModel->alias('a')->join('sys_image b', 'a.image_id = b.id')->paginate($size, false, $pageConfig);
-    
-        if (!$list) {
-            return ajax_error(ResultCode::E_DATA_NOT_FOUND, "广告不存在!");
+        $where = [];
+        if (!empty($title)) {
+            $where[] = ['title', 'like', "%$title%"];
+        }
+        if (!empty($soltIds)) {
+            $AdServingModel = new AdServingModel();
+            $adIds = $AdServingModel->where('slot_id', $soltIds)->field('distinct ad_id')->column('ad_id');//column变成一维数组
+            $where[] = ['id', 'in', $adIds];
         }
 
-        return ajax_return(ResultCode::ACTION_SUCCESS, '操作成功!', to_standard_pagelist($list));
+        $AdModel = new AdModel();
+        $list = $AdModel->where($where)->order('sort asc,id desc')->paginate($size, false, ['page'=>$page]);
+
+        foreach ($list as $ad) {
+            $ad['image'] = $this->findImage($ad);
+        }
+
+        $list = $list->toArray();
+        
+        //返回数据
+        $returnData['current'] = $list['current_page'];
+        $returnData['pages'] = $list['last_page'];
+        $returnData['size'] = $list['per_page'];
+        $returnData['total'] = $list['total'];
+        $returnData['data'] = parse_fields($list['data'], 1);
+
+        return ajax_return(ResultCode::ACTION_SUCCESS, '操作成功!', $returnData);
     }
 
     //查询广告插槽
@@ -88,24 +106,122 @@ class Ad extends Base
     //新增广告
     public function create()
     {
+        $params = $this->request->put();
 
+        //数据验证
+        $validate = new Validate();
+        $validate->rule([
+            'title' => 'require',
+            'url' => ['require','url'],
+            'slotIds' => ['require','array'],
+            'imageId' => ['require','integer']
+        ]);
+
+        if (!$validate->check($params)) {
+            return ajax_error(ResultCode::E_PARAM_VALIDATE_ERROR, $validate->getError());
+        }
+
+        $AdModel = new AdModel();
+        $params['create_time'] = date_time();
+        $soltIds = $params['slotIds'];
+        //驼峰转为下划线
+        $params = parse_fields($params);
+        $AdModel->isUpdate(false)->allowField(true)->save($params);
+        $adId = $AdModel->id;
+
+        if (!$adId) {
+            return ajax_return(ResultCode::E_DATA_VALIDATE_ERROR, '操作失败!');
+        }
+        //新增中间表数据
+        $pivot = ['update_time' => date_time(), 'create_time' => date_time()];
+        $AdModel->adSlots()->attach($soltIds, $pivot);
+
+        //返回数据
+        $ad = AdModel::get($adId);
+
+        $returnData = $ad;
+        $returnData['image'] = $this->findImage($ad);
+        
+        return ajax_return(ResultCode::ACTION_SUCCESS, '操作成功!', $returnData);
     }
 
     //编辑广告
-    public function audit()
+    public function edit()
     {
+        $params = $this->request->put();
 
+        //数据验证
+        $validate = new Validate();
+        $validate->rule([
+            'id' => 'require',
+            'title' => 'require',
+            'url' => 'require',
+            'slotIds' => ['require','array'],
+            'imageId' => ['require','integer']
+        ]);
+
+        if (!$validate->check($params)) {
+            return ajax_error(ResultCode::E_PARAM_VALIDATE_ERROR, $validate->getError());
+        }
+
+        $params['create_time'] = date_time();
+        $id = $params['id'];
+        $AdModel = new AdModel();
+        $rowsNum = $AdModel->isUpdate(true)->allowField(true)->save($params, ['id'=>$id]);
+
+        //更新中间表数据
+        $AdModel->adSlots()->detach();
+        $pivot = ['update_time' => date_time(), 'create_time' => date_time()];
+        $AdModel->adSlots()->attach($params['slotIds'], $pivot);
+
+        if ($rowsNum == false) {
+            return ajax_return(ResultCode::E_DATA_VALIDATE_ERROR, '操作失败!');
+        } 
+
+        //返回数据
+        $ad = AdModel::get($id);
+        $returnData = $ad;
+        $returnData['image'] = $this->findImage($ad);
+        
+        return ajax_return(ResultCode::ACTION_SUCCESS, '操作成功!', $returnData);
     }
 
     public function delete($id)
     {
         $ad = AdModel::get($id);
         if (!$ad) {
-            $this->error('分类不存在!');
+            $this->error('广告不存在!');
         }
 
         $ad->delete();
 
         return ajax_return(ResultCode::ACTION_SUCCESS, '操作成功!');
+    }
+
+    //查找广告图
+    public function findImage($ad)
+    {
+        $Image = [];
+        if (empty($ad['image_id']) || $ad['image_id'] == 0) {
+            return $Image;
+        }
+        $ImageModel = new ImageModel();
+        $Image = $ImageModel::get($ad['image_id']);
+    
+        if (empty($Image)) {
+            return $Image;
+        }
+
+        //完整路径
+        $Image['fullImageUrl'] = $ImageModel->getFullImageUrlAttr('',$Image);
+        $Image['FullThumbImageUrlAttr'] = $ImageModel->getFullThumbImageUrlAttr('',$Image);
+        unset($Image['remark']);
+        unset($Image['image_size']);
+        unset($Image['thumb_image_size']);
+        unset($ad['image_id']);
+
+        $Image = $Image->toArray();
+        $Image = parse_fields($Image,1);
+        return $Image;
     }
 }
