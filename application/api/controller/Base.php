c5fe58a8593899e0a5311f977828f98488666acc
外号猫哥 <cattong@163.com>
2021-12-08 13:55:34 +0800

    修复jwt的decode

--- application/api/controller/Base.php
+++ application/api/controller/Base.php
@@ -4,7 +4,8 @@ namespace app\api\controller;
 
 use think\Controller;
 use app\common\library\ResultCode;
-
+use Firebase\JWT\JWT;
+use think\facade\Log;
 class Base extends Controller
 {
 
@@ -18,7 +19,17 @@ class Base extends Controller
     }
 
     public function initialize() {
-        $this->user_info = session("jwt_payload_data");
+        $authorization = $this->request->header('authorization');
+        if (!empty($authorization)) {
+            $token = substr($authorization, 7);
+            $payload = null;
+            try {
+                $payload = JWT::decode($token, config('jwt.jwt_key'), [config('jwt.jwt_alg')]);
+                $this->user_info = $payload->data;
+            } catch (\Throwable $e) {
+                Log::error("jwt decode error:" . $e->getMessage());
+            }
+        }
     }
 
     //空操作：系统在找不到指定的操作方法的时候，会定位到空操作
@@ -26,18 +37,5 @@ class Base extends Controller
     {
         return ajax_error(ResultCode::SC_FORBIDDEN, '服务器拒绝请求或非法访问！');
     }
-
-    /**
-     * 测试生成sign
-     * @return string
-     */
-    // public function testMD5()
-    // {
-    //     $params = $this->request->put();
-    //     dump($params);
-    //     $signStr = arrToQuery($params, false);
-    //     $sign = strtoupper(md5($signStr . '&key=' . config('appkey')));
-    //     return $sign;
-    // }
 
 }
