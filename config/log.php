b55e5954f95aeb2c0b95835a6364cafc2804a21e
tangym <tangym@mytable.net>
2023-04-26 15:41:28 +0800

    提交确定的修改

--- config/log.php
+++ config/log.php
@@ -1,26 +1,45 @@
 <?php
-// +----------------------------------------------------------------------
-// | ThinkPHP [ WE CAN DO IT JUST THINK ]
-// +----------------------------------------------------------------------
-// | Copyright (c) 2006~2018 http://thinkphp.cn All rights reserved.
-// +----------------------------------------------------------------------
-// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
-// +----------------------------------------------------------------------
-// | Author: liu21st <liu21st@gmail.com>
-// +----------------------------------------------------------------------
 
 // +----------------------------------------------------------------------
 // | 日志设置
 // +----------------------------------------------------------------------
 return [
-    // 日志记录方式，内置 file socket 支持扩展
-    'type'  => 'app\\common\\thinkphp\\log\\driver\\File',
-    // 日志保存目录
-    'path'  => \think\facade\Env::get('runtime_path') . 'log' . DIRECTORY_SEPARATOR . 'system' . DIRECTORY_SEPARATOR,
-    //日志内的时间格式
-    'time_format' => 'Y-m-d H:i:s',
+    // 默认日志记录通道
+    'default'      => env('log.channel', 'file'),
     // 日志记录级别
-    'level' => ['error', 'notice', 'info', 'log', 'debug', 'sql'],
-    //独立日志
-    'apart_level'   =>  ['error'],
+    'level'        => [],
+    // 日志类型记录的通道 ['error'=>'email',...]
+    'type_channel' => [],
+    // 关闭全局日志写入
+    'close'        => false,
+    // 全局日志处理 支持闭包
+    'processor'    => null,
+
+    // 日志通道列表
+    'channels'     => [
+        'file' => [
+            // 日志记录方式
+            'type'           => 'app\\common\\thinkphp\\log\\driver\\File',
+            // 日志保存目录
+            'path'           => \think\facade\Env::get('runtime_path') . 'log' . DIRECTORY_SEPARATOR . 'system' . DIRECTORY_SEPARATOR,
+            // 单文件日志写入
+            'single'         => false,
+            // 独立日志级别
+            'apart_level'    => ['error'],
+            // 最大日志文件数量
+            'max_files'      => 0,
+            // 使用JSON格式记录
+            'json'           => false,
+            // 日志处理
+            'processor'      => null,
+            // 关闭通道日志写入
+            'close'          => false,
+            // 日志输出格式化
+            'format'         => '[%s][%s] %s',
+            // 是否实时写入
+            'realtime_write' => false,
+        ],
+        // 其它日志通道配置
+    ],
+
 ];
